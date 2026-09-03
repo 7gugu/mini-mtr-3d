916dedc7c72201abf7f963a354a9863daa4b489e
guguhuang <guguhuang@guguhuangdeiMac.local>
2026-09-03 23:52:31 +0800

    feat: 调整标题

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,6 @@
 // src/config.ts
-declare const process: {
-  env: {
-    [key: string]: string | undefined;
-  };
-};
-
 export const config = {
-  AMAP_KEY: process.env.AMAP_KEY || '',
-  AMAP_SECURITY_CODE: process.env.AMAP_SECURITY_CODE || '',
+    AMAP_KEY: process.env.AMAP_KEY || '',
+    AMAP_SECURITY_CODE: process.env.AMAP_SECURITY_CODE || '',
 };
 
